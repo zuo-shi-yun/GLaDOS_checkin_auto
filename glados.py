b4681f58e73ad53653ecc57cfa46fe8a8b2edce3
左诗云 <83642642+zuo-shi-yun@users.noreply.github.com>
2023-12-01 20:50:56 +0800

    Update glados.py

--- glados.py
+++ glados.py
@@ -33,10 +33,10 @@ if __name__ == '__main__':
             print(email+'----结果--'+mess+'----剩余('+time+')天')  # 日志输出
             sendContent += email+'----'+mess+'----剩余('+time+')天\n'
         else:
-            requests.get('http://www.pushplus.plus/send?token=' + sckey + '&content='+email+'cookie已失效')
+            requests.get('http://www.pushplus.plus/send?token=' + sckey + '&content='+email+'cookie已失效'+"&channel=mail")
             print('cookie已失效')  # 日志输出
      #--------------------------------------------------------------------------------------------------------#   
     if sckey != "":
-         requests.get('http://www.pushplus.plus/send?token=' + sckey + '&title='+email+'签到成功'+'&content='+sendContent)
+         requests.get('http://www.pushplus.plus/send?token=' + sckey + '&title='+email+'签到成功'+'&content='+sendContent+"&channel=mail")
 
 
